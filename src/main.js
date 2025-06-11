16b44698043b23949ce26abf1a333ddb5462413c
SungMin-hub <sungmin806@gmail.com>
2025-06-11 17:13:17 +0900

    로그인 기능 주석

--- src/main.js
+++ src/main.js
@@ -17,48 +17,48 @@ if (!auth.accessToken && localStorage.getItem('access_token')) {
   auth.setAccessToken(localStorage.getItem('access_token'))
 }
 
-const tryRefreshAndLoadUser = async () => {
-  if (auth.accessToken && !auth.userInfo) {
-    // accessToken은 있으나 userInfo가 없는 경우 → 마이페이지 호출
-    try {
-      const res = await api.get('/employee/mypage', {
-        headers: {
-          Authorization: `Bearer ${auth.accessToken}`
-        },
-        withCredentials: true
-      })
+// const tryRefreshAndLoadUser = async () => {
+//   if (auth.accessToken && !auth.userInfo) {
+//     // accessToken은 있으나 userInfo가 없는 경우 → 마이페이지 호출
+//     try {
+//       const res = await api.get('/employee/mypage', {
+//         headers: {
+//           Authorization: `Bearer ${auth.accessToken}`
+//         },
+//         withCredentials: true
+//       })
       
-      auth.setUserInfo(res.data)
-    } catch (err) {
-      console.error('[userInfo 복구 실패]', err)
-      auth.clearToken()
-    }
-  } else if (!auth.accessToken) {
-    // accessToken도 없는 경우 → refreshToken으로 재발급
-    try {
-      const res = await api.post('/auth/refresh-token', null, {
-        withCredentials: true
-      })
-      const newToken = res.headers['authorization']?.split(' ')[1]
+//       auth.setUserInfo(res.data)
+//     } catch (err) {
+//       console.error('[userInfo 복구 실패]', err)
+//       auth.clearToken()
+//     }
+//   } else if (!auth.accessToken) {
+//     // accessToken도 없는 경우 → refreshToken으로 재발급
+//     try {
+//       const res = await api.post('/auth/refresh-token', null, {
+//         withCredentials: true
+//       })
+//       const newToken = res.headers['authorization']?.split(' ')[1]
 
-      if (newToken) {
-        auth.setAccessToken(newToken)
+//       if (newToken) {
+//         auth.setAccessToken(newToken)
 
-        const userRes = await api.get('/employee/mypage', {
-          headers: {
-            Authorization: `Bearer ${newToken}`
-          },
-          withCredentials: true
-        })
-        auth.setUserInfo(userRes.data)
-      }
-    } catch (err) {
-      console.warn('[refresh-token 재발급 실패]', err)
-      auth.clearToken()
-      window.location.href = '/login'
-    }
-  }
-}
+//         const userRes = await api.get('/employee/mypage', {
+//           headers: {
+//             Authorization: `Bearer ${newToken}`
+//           },
+//           withCredentials: true
+//         })
+//         auth.setUserInfo(userRes.data)
+//       }
+//     } catch (err) {
+//       console.warn('[refresh-token 재발급 실패]', err)
+//       auth.clearToken()
+//       window.location.href = '/login'
+//     }
+//   }
+// }
 
 app.mount('#app')
 
