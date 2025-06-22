8f4f0924557f108d11810dca76d76a911aa2eade
Bluesky3125 <charlie4822@gmail.com>
2025-06-22 15:09:35 +0900

    Feat: 사원별 실적 대시보드 페이지 생성

--- src/config/menuConfig.js
+++ src/config/menuConfig.js
@@ -5,13 +5,22 @@ export const menuList = [
       {
         label: "사원별 실적 조회",
         path: "/dashboard/employee",
-        // component: () =>
-        // import("@/views/dashboard/EmployeePerformanceView.vue"),
+        redirect: "/dashboard/employee/:employeeCode",
+        component: () => import("@/views/dashboard/EmployeeDashboard.vue"),
+        meta: {
+          title: "사원별 대시보드",
+          basePath: "/dashboard/employee"
+        }
       },
       {
         label: "상품별 매출 조회",
         path: "/dashboard/product",
-        // component: () => import("@/views/dashboard/ProductSalesView.vue"),
+        redirect: "/dashboard/product/:productId",
+        component: () => import("@/views/dashboard/ProductDashboard.vue"),
+        meta: {
+          title: "상품병 대시보드",
+          basePath: "/dashboard/product"
+        }
       },
       {
         label: "영업부 매출 조회",
