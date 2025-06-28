3bd75437975af69501ba2013ddc9c693998d3915
jakego1 <jakego13129@gmail.com>
2025-06-28 11:46:53 +0900

    경로 수정

--- src/views/log/AccessLogView.vue
+++ src/views/log/AccessLogView.vue
@@ -52,8 +52,8 @@ const displayedLogs = ref([])
 
 onMounted(async () => {
   // const res = await axios.get('http://localhost:5000/api/log/access')
-  const res = await axios.get('http://localhost:5001/api/log/access')
-  // const res = await axios.get('https://api.saladerp.com/api/log/access')
+  // const res = await axios.get('http://localhost:5001/api/log/access')
+  const res = await axios.get('https://api.saladerp.com/api/log/access')
   logs.value = res.data
   displayedLogs.value = logs.value // 최초 전체 표시
 })
